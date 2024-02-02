f9b9a2a4be2a9551c303ebc128424aa50acebd77
mahnooshr <74074818+mahnooshr@users.noreply.github.com>
2024-02-02 17:24:04 +0330

    Add files via upload

---
+++ project/frontend/src/pages/chat.js
@@ -0,0 +1,8 @@
+// Toggle chat popup
+document.getElementById("chat-btn").addEventListener("click", function() {
+    document.getElementById("chat-popup").style.display = "block";
+  })
+  
+  document.getElementById("chat-close").addEventListener("click", function() {
+    document.getElementById("chat-popup").style.display = "none";
+  })
