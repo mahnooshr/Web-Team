cf72ec90e299e37e1ad8963a2c142892655edfe2
soligits <m.soltani.252@gmail.com>
2024-02-02 12:45:44 +0330

    update

--- homeworks/HW3/src/locust/locustfile.py
+++ homeworks/HW3/src/locust/locustfile.py
@@ -1,3 +1,12 @@
-from locust import HttpUser, between, task
+from locust import HttpUser, task, between
 
-# TODO PART 5
+class MyUser(HttpUser):
+    wait_time = between(1, 5)
+
+    @task
+    def access_server1(self):
+        self.client.get("http://localhost:8000")
+
+    @task
+    def access_server2(self):
+        self.client.get("http://localhost:8001")
