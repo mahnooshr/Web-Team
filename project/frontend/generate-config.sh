70cfb6c9816e66af3294bb040979050836fd84bb
soligits <m.soltani.252@gmail.com>
2024-02-04 13:37:26 +0330

    fix docker compose bug

--- project/frontend/generate-config.sh
+++ project/frontend/generate-config.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-envsubst '$BACKEND_HOST' &lt; /etc/nginx/conf.d/custom-nginx.template &gt; /etc/nginx/conf.d/default.conf;
+envsubst '$BACKEND_HOST' < /etc/nginx/conf.d/custom-nginx.template > /etc/nginx/conf.d/default.conf;
 exec nginx -g "daemon off;";
