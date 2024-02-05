3f0f80e8ba916054e04c36162e9c4762770ca7e8
soligits <m.soltani.252@gmail.com>
2024-02-05 16:00:15 +0330

    fix icon buttons

--- project/frontend/src/components/icon_button.js
+++ project/frontend/src/components/icon_button.js
@@ -1,10 +1,11 @@
 import React from "react";
 import '../styles/icon_button.css';
+import '../styles/card.css';
 
 function IconButton(props) {
     return (
-        <div className={props.className + 'icon-button'} onClick={props.onClick}>
-            <img src={props.icon} alt={props.alt} width={50} height={50} />
+        <div className={'icon-button ' + props.className} onClick={props.onClick}>
+            <img src={props.icon} alt={props.alt}/>
         </div>
     );
 }
