f3588ac63ab80a7413986d322ea61a40acf1a68e
mahnooshr <mahnoosh_mr79@yahoo.com>
2024-02-05 10:13:11 +0330

    cart.js

--- project/frontend/src/pages/cart.js
+++ project/frontend/src/pages/cart.js
@@ -1,10 +1,23 @@
-import React from "react";
-import '../styles/pages_general.css';
+import { useState } from 'react';
 
-export default function Cart(){
-    return (
-        <div className="container">
-            <h1>Cart</h1>
-        </div>
-    );
+export default function Cart() {
+
+  const [cartItems, setCartItems] = useState([]);
+
+  function addToCart(product) {
+    setCartItems(prevItems => [...prevItems, product]);
+  }
+
+  return (
+    <div className="cart">
+      {cartItems.map(item => (
+        <CartItem product={item} />
+      ))}
+    </div>
+  );
+
+}
+
+function CartItem({ product }) {
+  // display product data
 }
